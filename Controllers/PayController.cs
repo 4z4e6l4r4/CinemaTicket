4f190fc0a074b57e0abdc268b5b5aa1d6c4409ce
Azra <yukselazrael@gmail.com>
2024-01-25 09:17:57 +0300

    Added Controller

--- Controllers/PayController.cs
+++ Controllers/PayController.cs
@@ -1,83 +1,153 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using CinemaTicket.Data;
+using CinemaTicket.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace CinemaTicket.Controllers
 {
     public class PayController : Controller
     {
-        // GET: PayController
-        public ActionResult Index()
+        private readonly ApplicationDbContext _context;
+
+        public PayController(ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        // GET: Pay
+        public async Task<IActionResult> Index()
+        {
+            var payments = _context.Pays.Include(p => p.Ticket);
+            return View(await payments.ToListAsync());
+        }
+
+        // GET: Pay/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pay = await _context.Pays
+                .Include(p => p.Ticket)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pay == null)
+            {
+                return NotFound();
+            }
+
+            return View(pay);
+        }
+
+        // GET: Pay/Create
+        public IActionResult Create()
+        {
+            // Bilet bilgilerini dropdown list için yükle
             return View();
         }
 
-        // GET: PayController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: PayController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: PayController/Create
+        // POST: Pay/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public async Task<IActionResult> Create([Bind("Id,TicketId,PaymentAmount,PaymentMethod,PaymentStatus,IsStatus,IsDelete")] Pay pay)
         {
-            try
+            if (ModelState.IsValid)
             {
+                _context.Add(pay);
+                await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            // Bilet bilgilerini dropdown list için yükle
+            return View(pay);
         }
 
-        // GET: PayController/Edit/5
-        public ActionResult Edit(int id)
+        // GET: Pay/Edit/5
+        public async Task<IActionResult> Edit(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pay = await _context.Pays.FindAsync(id);
+            if (pay == null)
+            {
+                return NotFound();
+            }
+            // Bilet bilgilerini dropdown list için yükle
+            return View(pay);
         }
 
-        // POST: PayController/Edit/5
+        // POST: Pay/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TicketId,PaymentAmount,PaymentMethod,PaymentStatus,IsStatus,IsDelete")] Pay pay)
         {
-            try
+            if (id != pay.Id)
             {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pay);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PayExists(pay.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
                 return RedirectToAction(nameof(Index));
             }
-            catch
-            {
-                return View();
-            }
+            // Bilet bilgilerini dropdown list için yükle
+            return View(pay);
         }
 
-        // GET: PayController/Delete/5
-        public ActionResult Delete(int id)
+        // GET: Pay/Delete/5
+        public async Task<IActionResult> Delete(int? id)
         {
-            return View();
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pay = await _context.Pays
+                .Include(p => p.Ticket)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pay == null)
+            {
+                return NotFound();
+            }
+
+            return View(pay);
         }
 
-        // POST: PayController/Delete/5
-        [HttpPost]
+        // POST: Pay/Delete/5
+        [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            var pay = await _context.Pays.FindAsync(id);
+            _context.Pays.Remove(pay);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PayExists(int id)
+        {
+            return _context.Pays.Any(e => e.Id == id);
         }
     }
 }
